d2d66748efc96ded9174ece2d5f36aa05f580ec5
Ariel <ar.i117@hotmail.com>
2017-03-13 11:07:44 -0600

    Deque comments on class

--- PA2/Deque.java
+++ PA2/Deque.java
@@ -14,38 +14,47 @@ public class Deque<Item> implements Iterable<Item> {
         }
     }
     
+    // construct an empty deque
     public Deque() {
         size = 0;
         head = new Node<Item>(null);
         head.next = null;
     }
     
+    // @return true if deque is empty
     public boolean isEmpty() {
         return true;
     }
     
+    // @return the number of elements on the deque
     public int size() {
-        return 0;
+        return size;
     }
     
+    // add the item to the front
     public void addFirst(Item item) {
     }
     
+    // add the item to the end
     public void addLast(Item item) {
     }
     
+    // remove and return the item from the front
     public Item removeFirst() {
         return null;
     }
     
+    // remove and return the item from the end
     public Item removeLast() {
         return null;
     }
     
+    // return an iterator over items in order from front to end
     public Iterator<Item> iterator() {
         return new ListIterator();
     }
     
+    //  class the implements the Iterator interface
     private class ListIterator implements Iterator<Item> {
         public boolean hasNext() {
             return false;
@@ -56,7 +65,8 @@ public class Deque<Item> implements Iterable<Item> {
             return null;
         }
     }
-     
+    
+    // main for testing
     public static void main(String[] args) {
     }
 }
