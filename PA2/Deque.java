08987919e167ba93962da94fccea17ed6da2f3c2
Ariel <ar.i117@hotmail.com>
2017-03-13 23:36:26 -0600

    Refactored to a doubly linkedlist and addFirst implemented

--- PA2/Deque.java
+++ PA2/Deque.java
@@ -3,14 +3,17 @@ import java.util.Iterator;
 public class Deque<Item> implements Iterable<Item> {
     private int size;
     private Node<Item> head;
+    private Node<Item> tail;
     
     class Node<Item> {
         Node<Item> next;
+        Node<Item> prev;
         Item data;
         
         public Node(Item data) {
             this.data = data;
             this.next = null;
+            this.prev = null;
         }
     }
     
@@ -18,12 +21,13 @@ public class Deque<Item> implements Iterable<Item> {
     public Deque() {
         size = 0;
         head = new Node<Item>(null);
-        head.next = null;
+        head.next = tail;
+        tail.prev = head;
     }
     
     // @return true if deque is empty
     public boolean isEmpty() {
-        return true;
+        return (head.next == tail) && (tail.prev == head);
     }
     
     // @return the number of elements on the deque
@@ -33,15 +37,38 @@ public class Deque<Item> implements Iterable<Item> {
     
     // add the item to the front
     public void addFirst(Item item) {
+        Node<Item> node = new Node<Item>(item);
+        if (size == 0) {
+            // set the pointers of the new node
+            node.prev = tail.prev;
+            node.next = head.next;
+            // rearrange the pointers of head and tail
+            head.next = node;
+            tail.prev = node;
+        } 
+        else {
+            // set the pointers of the new node
+            node.next = head.next;
+            node.prev = head;
+            // rearrage the pointers of head and head + 1
+            head.next.prev = node;
+            head.next = node;
+        }
+        size += 1;
     }
     
     // add the item to the end
     public void addLast(Item item) {
+        Node<Item> node = new Node<Item>(item);
+        tail.next = node;
+        tail = node;
     }
     
     // remove and return the item from the front
     public Item removeFirst() {
-        return null;
+        Item data = head.data;
+        head = head.next;
+        return data;
     }
     
     // remove and return the item from the end
