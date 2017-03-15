552ad1d4d6387af1aa6fbb17116d6975a6841f13
Ariel <ar.i117@hotmail.com>
2017-03-14 22:40:10 -0600

    addLast, removeFirst, removeLast - implemented

--- PA2/Deque.java
+++ PA2/Deque.java
@@ -1,16 +1,20 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
+    /**
+     Doubly linkedlist structure with sentinel nodes.
+     */
     private int size;
     private Node<Item> head;
     private Node<Item> tail;
     
-    class Node<Item> {
-        Node<Item> next;
-        Node<Item> prev;
-        Item data;
+    class Node<E> {
+        Node<E> next;
+        Node<E> prev;
+        E data;
         
-        public Node(Item data) {
+        public Node(E data) {
             this.data = data;
             this.next = null;
             this.prev = null;
@@ -21,6 +25,7 @@ public class Deque<Item> implements Iterable<Item> {
     public Deque() {
         size = 0;
         head = new Node<Item>(null);
+        tail = new Node<Item>(null);
         head.next = tail;
         tail.prev = head;
     }
@@ -60,20 +65,47 @@ public class Deque<Item> implements Iterable<Item> {
     // add the item to the end
     public void addLast(Item item) {
         Node<Item> node = new Node<Item>(item);
-        tail.next = node;
-        tail = node;
+        // set new node next and prev pointers
+        node.next = tail;
+        node.prev = tail.prev;
+        if (size == 0) {
+            // rearrange head and tail pointers
+            head.next = node;
+            tail.prev = node;
+        } 
+        else {
+            // rearrange next and prev pointers of previous node
+            // and from tail node
+            tail.prev.next = node;
+            tail.prev = node;
+        }
+        size += 1;
     }
     
     // remove and return the item from the front
     public Item removeFirst() {
-        Item data = head.data;
-        head = head.next;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        // save item and delete next node
+        Item data = head.next.data;
+        head.next.next.prev = head;
+        head.next = head.next.next;
+        
         return data;
     }
     
     // remove and return the item from the end
     public Item removeLast() {
-        return null;
+        if (size == 0) 
+            throw new NoSuchElementException();
+        
+        // save item and delete previous node
+        Item data = tail.prev.data;
+        tail.prev.prev.next = tail;
+        tail.prev = tail.prev.prev;
+
+        return data;
     }
     
     // return an iterator over items in order from front to end
